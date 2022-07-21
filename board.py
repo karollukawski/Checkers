91b0679ecbf1e1822b49eb0491ae29b1121736f7
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-07-21 22:08:50 +0200

    Added board file

---
+++ board.py
@@ -0,0 +1,10 @@
+import pygame 
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
+
+    
