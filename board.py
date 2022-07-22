ef9e6cf562e12f9950c3c84c6bafae2381e75637
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-07-22 09:31:55 +0200

    Updated board and main function

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import pygame 
+from constans import BLACK, ROWS, RED, SQUARE_SIZE
 
 class Board:
     def __init__(self):
@@ -7,4 +8,8 @@ class Board:
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
 
-    
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        for row in range (ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
