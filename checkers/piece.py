6ebb08d8e73176ed3a8d641f581e675a8eaaabea
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-08-03 23:18:43 +0200

    Updated draw function

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,5 +1,5 @@
 import pygame
-from constans import RED, WHITE, SQUARE_SIZE, GREY
+from constans import RED, WHITE, SQUARE_SIZE, GREY, CROWN
 
 class Piece:
     
@@ -10,7 +10,7 @@ class Piece:
         self.row = row
         self.col = col
         self.color = color
-        self.color = False
+        self.color = True
 
         if self.color == RED:
             self.direction = -1
@@ -32,6 +32,9 @@ class Piece:
         radius = SQUARE_SIZE//2 - self.PADDING
         pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
     
     def __repr__(self):
         return str(self.color)
