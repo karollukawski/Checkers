7620a7a6bb098a070a25071bbabd0092624fcfb5
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-08-09 20:27:56 +0200

    Everything worked - moving by using mouse

--- main.py
+++ main.py
@@ -18,17 +18,18 @@ def main():
     clock = pygame.time.Clock()
     board = Board()
 
-    piece = board.get_piece(0, 1)
-    board.move(piece, 4, 3)
-
     while run:
         clock.tick(FPS)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move(piece, 4, 3)
 
         board.draw(WIN)
         pygame.display.update()
