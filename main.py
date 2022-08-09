7ee2af1596851a0c4c970833d2ae1e4cef4da977
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-08-09 19:17:34 +0200

    Everything worked - moving only by using keyboard

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main():
     clock = pygame.time.Clock()
     board = Board()
 
-    board.move()
+    piece = board.get_piece(0, 1)
+    board.move(piece, 4, 3)
 
     while run:
         clock.tick(FPS)
