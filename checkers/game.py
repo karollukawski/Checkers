b2a72ad79194ddbea4815f4de8aebd7df6ebb475
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-08-11 00:54:41 +0200

    Organizing methods in game file

--- checkers/game.py
+++ checkers/game.py
@@ -4,12 +4,18 @@ from .board import Board
 
 class Game:
     def __init__(self, win):
-        self.selected = None
-        self.board = Board()
-        self.turn = RED
-        self.valid_moves = {}
+        self._init()
         self.win = win
 
     def update(self):
         self.board.draw(self.win)
         pygame.display.update()
+
+    def _init(self):
+        self.selected = None
+        self.board = Board()
+        self.turn = RED
+        self.valid_moves = {}
+    
+    def reset(self):
+        self._init()
