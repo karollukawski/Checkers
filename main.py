e0fc7ade247ac14f815ef9a136a893b5d16a9a04
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-08-10 23:26:34 +0200

    Testing new function, but sth is wrong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from checkers.constans import SQUARE_SIZE, WIDTH, HEIGHT
 from checkers.board import Board
+from checkers.game import Game
 
 FPS = 60
 
@@ -16,7 +17,7 @@ def get_row_col_from_mouse(pos):
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
@@ -28,11 +29,8 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                piece = board.get_piece(row, col)
-                board.move(piece, 4, 3)
 
-        board.draw(WIN)
-        pygame.display.update()
+        game.update()
     
     pygame.quit()
 
