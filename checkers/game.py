66e9ef8b729ac829a75c0aef5cbb5700635a4f0e
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-08-26 19:19:14 +0200

    Some corrections to make game working correctly

--- checkers/game.py
+++ checkers/game.py
@@ -27,12 +27,12 @@ class Game:
             if not result:
                 self.selected = None
                 self.selected(row, col)
-        else:
-            piece = self.board.get_piece(row, col)
-            if piece != 0 and piece.color == self.turn:
-                self.selected = piece
-                self.valid_moves = self.board.get_valid_moves(piece)
-                return True
+        
+        piece = self.board.get_piece(row, col)
+        if piece != 0 and piece.color == self.turn:                
+            self.selected = piece
+            self.valid_moves = self.board.get_valid_moves(piece)
+            return True
 
         return False
 
@@ -40,6 +40,9 @@ class Game:
         piece = self.board.get_piece(row, col)
         if self.selected and piece == 0 and (row, col) in self.valid_moves:
             self.board.move(self.selected, row, col)
+            skipped = self.valid_moves[(row, col)]
+            if skipped:
+                self.board.remove(skipped)
             self.change_turn()
         else:
             return False
